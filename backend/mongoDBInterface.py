1aa66aee2771dd6c4ad18b60529516d45beb6b98
saddboys <cdavidnz@gmail.com>
2020-08-29 14:34:45 +1200

    added methods to get collection and make new db in mongodb

--- backend/mongoDBInterface.py
+++ backend/mongoDBInterface.py
@@ -5,12 +5,29 @@ import pymongo
 
 from dotenv import load_dotenv
 
-def get_col(col):
+
+def get_db_client():
     load_dotenv()
     ATLAS_URI = os.getenv("ATLAS_URI")
     myclient = pymongo.MongoClient(ATLAS_URI)
-    return myclient["Test"][col]
+    return myclient
+
+
+def get_col(proj, col):
+    myclient = get_db_client()
+    return myclient[proj][col]
+
+
+def create_db_for_proj(proj_name):
+    myclient = get_db_client()
+    mydb = myclient[proj_name]
+    mydb.create_collection("users")
+    mydb.create_collection("documents")
+    mydb.create_collection("labels")
+
 
 if __name__ == '__main__':
-    mycol = get_col("Labels")
+    mycol = get_col("Test", "Labels")
+    print(mycol.find_one())
+    create_db_for_proj("Test")
 
