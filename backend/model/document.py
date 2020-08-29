b4ec05d6277561d23f0389071a2f60f864adb192
saddboys <cdavidnz@gmail.com>
2020-08-29 14:46:26 +1200

    setup backend model

---
+++ backend/model/document.py
@@ -0,0 +1,10 @@
+class Document:
+
+    def __init__(self, identifier, data):
+        self.identifier = identifier
+        self.data = data
+        self.user_and_labels = {}
+
+    def add_user_and_label(self, user, label):
+        self.user_and_labels.update({user.email, label})
+        # push changes to mongodb
