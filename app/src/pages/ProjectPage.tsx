bfbfdf2bd3c81b9515a739e8c5e48e5b240b50e3
In Ha Ryu <iryu815@aucklanduni.ac.nz>
2020-08-31 21:45:34 +1200

    empty commit

--- app/src/pages/ProjectPage.tsx
+++ app/src/pages/ProjectPage.tsx
@@ -55,7 +55,7 @@ const ProjectPage: React.FC = () => {
   }
 
   const changeTag = (i:number, label:string) => {
-    //TODO: connect with backend to update tags
+    //TODO: connect with backend to update tags s
     documents[i].tag = label
     setShowModal(false)
   }
