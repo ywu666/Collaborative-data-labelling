3a783caee446a28758c63f95d96c7527bd90d254
tnzhu <tzhu442@aucklanduni.ac.nz>
2020-09-01 10:56:05 +1200

    Added tests for adding document and updating map of user/assigned label

--- backend/tests/test_document.py
+++ backend/tests/test_document.py
@@ -1,6 +1,10 @@
 import unittest
 
+from backend import mongoDBInterface
 from backend.model.document import Document
+from backend.model.label import Label
+from backend.model.user import User
+
 
 class TestDocument(unittest.TestCase):
 
@@ -8,6 +12,28 @@ class TestDocument(unittest.TestCase):
     def test_setup_document(self):
         my_document = Document(1, "data")
         assert(my_document.identifier == 1 and my_document.data == "data")
+
+    def test_add_document(self):
+        my_document = Document(3, "data")
+        col = mongoDBInterface.get_col("Test", "documents")
+        my_document.add_new_document()
+        assert (col.find_one({"identifier": 3}))
+
+    def test_add_user_and_label(self):
+        my_document = Document(2, "test data")
+        # Need to put document in database
+        my_document.add_new_document()
+
+        user = User("User 1", "Last name", "testingDoc@testmail.com", [])
+        label = Label("Document")
+
+        my_document.add_user_and_label(2, user, label.__dict__)
+
+        col = mongoDBInterface.get_col("Test", "documents")
+        document_in_database = col.find_one({"identifier": 2})
+
+        assert (user.email in document_in_database["user_and_labels"])
+        assert (document_in_database["user_and_labels"][user.email] == label.__dict__)
 
 
 if __name__ == '__main__':
