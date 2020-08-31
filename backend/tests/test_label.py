d0efc7f540f34c4d51f1d28ab313ea3e268eb92d
tnzhu <tzhu442@aucklanduni.ac.nz>
2020-08-31 22:47:41 +1200

    Added test for persisting of data when new label is created

--- backend/tests/test_label.py
+++ backend/tests/test_label.py
@@ -1,7 +1,21 @@
+import unittest
+
+from backend import mongoDBInterface
 from backend.model.label import Label
 
 
-# Tests whether or not setting up a label works
-def test_setup_label():
-    my_label = Label("Music")
-    assert(my_label.name == "Music")
+class LabelTest(unittest.TestCase):
+    # Tests whether or not setting up a label works
+    def test_setup_label(self):
+        my_label = Label("Music")
+        assert (my_label.name == "Music")
+
+    # Tests if data is persisted to database when new label created
+    def test_new_lable_persisted(self):
+        my_label = Label("New Label")
+        col = mongoDBInterface.get_col("Test", "lables")
+        assert (col.find(my_label.__dict__))
+
+
+if __name__ == '__main__':
+    unittest.main()
