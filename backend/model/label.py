d3b54c23fdb6a4caf70f8e40dbc1f779079b2197
tnzhu <tzhu442@aucklanduni.ac.nz>
2020-08-31 22:48:08 +1200

    Added method for persisting data upon creation of label

--- backend/model/label.py
+++ backend/model/label.py
@@ -1,5 +1,15 @@
+from backend import mongoDBInterface
+
+
 class Label:
 
     def __init__(self, name):
         self.name = name
+
+        self.add_new_label
+
+    # Method called to persist label to database
+    def add_new_label(self):
+        col = mongoDBInterface.get_col("Test", "labels")
+        col.insert_one(self.__dict__)
 
