08914463d2669bacaaa0aeafa22403d00fb442a3
Maggie Liu <maggieqliu81@gmail.com>
2020-08-31 20:57:25 +1200

    Style the sign in page

--- app/src/pages/Page.tsx
+++ app/src/pages/Page.tsx
@@ -12,6 +12,12 @@ import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
 import { LoginInput } from '../components/LoginInput';
+import { css } from 'glamor';
+import "../components/ExploreContainer.css"
+const container = css({
+  
+  // justifyContent: 'center',
+});
 
 const Page: React.FC = () => {
   const { name } = useParams<{ name: string }>();
@@ -27,15 +33,18 @@ const Page: React.FC = () => {
         </IonToolbar>
       </IonHeader>
 
-      <IonContent>
+    
+      <IonContent >
         <IonHeader collapse="condense">
           <IonToolbar>
             <IonTitle size="large">{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
-        {/* <ExploreContainer name={name} /> */}
-        <LoginInput />
+        <div className="container">
+        <LoginInput {...container} />
+        </div>
       </IonContent>
+     
     </IonPage>
   );
 };
