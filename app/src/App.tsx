cc26f55afb141482b2d57b9dbc44676c276485e6
AXon <seismiccat@protonmail.com>
2020-08-31 23:38:48 +1200

    added settings page to router

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,5 +1,6 @@
 import Menu from './components/Menu';
 import Page from './pages/Page';
+import SettingsPage from './pages/SettingsPage';
 import React from 'react';
 import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
@@ -28,13 +29,9 @@ const App: React.FC = () => {
   return (
     <IonApp>
       <IonReactRouter>
-        <IonSplitPane contentId="main">
-          <Menu />
-          <IonRouterOutlet id="main">
-            <Route path="/page/:name" component={Page} exact />
-            <Redirect from="/" to="/page/Inbox" exact />
+        <IonRouterOutlet id="main">
+            <Route path="/page/Settings" component={SettingsPage} exact />
           </IonRouterOutlet>
-        </IonSplitPane>
       </IonReactRouter>
     </IonApp>
   );
