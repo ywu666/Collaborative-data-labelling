b65f1d69776088b68977fb2740eb82ac9c7f2786
Maggie Liu <maggieqliu81@gmail.com>
2020-09-01 12:04:48 +1200

    Add login component tests

--- app/src/pages/LoginPage.tsx
+++ app/src/pages/LoginPage.tsx
@@ -9,7 +9,6 @@ import {
 } from '@ionic/react';
 import React from 'react';
 import { useParams } from 'react-router';
-import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
 import { LoginInput } from '../components/LoginInput';
 import "../components/Login.css"
