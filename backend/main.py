5c8acdcdef6412a59c151f62cc3aa105e54f71d5
Rajan Gupta <RajanGuptaNZ@gmail.com>
2020-09-01 13:14:25 +1200

    Added __init__.py file in /model to allow backend to use model classes

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,6 @@
-from flask import Flask
+from flask import Flask, request, make_response
 from flask_cors import CORS
+from backend.model.label import Label
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -8,6 +9,40 @@ cors = CORS(app)
 @app.route('/hello', methods=['Get'])
 def hello():
     return "hello world"
+
+@app.route('/addlabel', methods=['Post'])
+def addLabel():
+    if 'projectID' in request.json:
+        projectID = int(request.json['projectID'])
+    else:
+        response = {'status_code': 400,
+                    'message': "Missing projectID"}
+        response = make_response(response)
+        return response
+
+    if 'userID' in request.json:
+        userID = int(request.json['userID'])
+    else:
+        response = {'status_code': 400,
+                    'message': "Missing userID"}
+        response = make_response(response)
+        return response
+
+    if 'label' in request.json:
+        label = Label(request.json['label'])
+    else:
+        response = {'status_code': 400,
+                    'message': "Missing label"}
+        response = make_response(response)
+        return response
+
+    ### Check user permisisions for project ###
+    ### Add to DB ###
+
+    response = {'status_code': 200,
+                'message': "Added label succesfully"}
+    response = make_response(response)
+    return response
 
 
 if __name__ == '__main__':
