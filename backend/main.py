625dd5e08ecf6ddc49bd389506148cb574058034
tnzhu <tzhu442@aucklanduni.ac.nz>
2020-09-02 10:58:49 +1200

    Added route for uploading files to the backend

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,10 @@
-from flask import Flask
+from bson import ObjectId
+from flask import Flask, request, make_response
 from flask_cors import CORS
+from numpy.core.defchararray import isnumeric
+
+from backend.model.document import Document
+from backend.model.project import Project
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -8,6 +13,43 @@ cors = CORS(app)
 @app.route('/hello', methods=['Get'])
 def hello():
     return "hello world"
+
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'projectID' in request.json:
+            project_id = ObjectId(request.json['projectID'])
+
+            if 'inputFile' not in request.files:
+                response = {'status_code': 400,
+                            'message': 'No file selected'}
+                response = make_response(response)
+                return response
+
+            file = request.files['inputFile']
+            if file.filename == '':
+                response = {'status_code': 400,
+                            'message': 'No file selected'}
+                response = make_response(response)
+                return response
+            if file:
+                documents_to_import = file.readlines()
+                documents = []
+
+                for d in documents_to_import:
+                    if isnumeric(d[0]):
+                        documents.append(Document(int(d[0]), d[1]))
+
+                # Find project database and populate document collection
+                project = Project(project_id, [], [])
+                project.add_documents(documents)  # need to write method for multidoc population
+
+        else:
+            response = {'status_code': 400,
+                        'message': 'No project id provided'}
+            response = make_response(response)
+            return response
 
 
 if __name__ == '__main__':
