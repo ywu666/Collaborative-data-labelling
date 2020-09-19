6b1abca797f25110d35b227c55647366ad510771
saddboys <cdavidnz@gmail.com>
2020-09-19 16:20:49 +1200

    merged from master

--- backend/main.py
+++ backend/main.py
@@ -72,6 +72,31 @@ def upload_file():
             response = make_response(response)
             return response
 
-
+#endpoint to add/delete/get preset labels
+@app.route('/presetlabels', methods=['POST', 'GET', 'DELETE'])
+def presetLabels():
+    requestPresent = False
+    if (request.method == 'GET'):
+        return Project.preset_labels
+    for labels in Project.preset_labels:
+        if (labels == Label(request.json['label'])):
+            requestPresent = True
+    if (request.method == 'POST'):
+        if requestPresent:
+            response = {'status_code': 400, 'message':"Label already set"}
+        else:
+            response = {'status_code': 200, 'message':"Added label successfully"}
+            Project.preset_labels.append(request.json['label'])
+        response = make_response(response)
+        return response
+    if(request.method == 'DELETE'):
+        if requestPresent:
+            Project.preset_labels.remove(request.json['label'])
+            response = {'status_code': 200, 'message': "Label deleted successfully"}
+        else:
+            response = {'status_code': 400, 'message': "Label was not set"}
+        response = make_response(response)
+        return response
+                
 if __name__ == '__main__':
     app.run()
