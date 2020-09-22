c76fe0ebbb9682464b5024b96519d4ab0a0cc546
saddboys <cdavidnz@gmail.com>
2020-09-22 13:24:53 +1200

    fixed rebase conflict from master

--- backend/api/project_api.py
+++ backend/api/project_api.py
@@ -1,12 +1,26 @@
 from flask import Blueprint, request, make_response
 
-import mongoDBInterface
+from mongoDBInterface import create_db_for_proj, get_col, get_db_client
+from firebase_auth import get_email
 
 project_api = Blueprint('project_api', __name__)
 
 
 @project_api.route("/projects", methods=['POST'])
 def create_project():
+    if 'id_token' in request.json:
+        id_token = request.json['id_token']
+
+        requestor_email = get_email(id_token)
+        if requestor_email is None:
+            response = {'message': "ID Token has expired or is invalid"}
+            response = make_response(response)
+            return response, 400
+    else:
+        response = {'message': "Missing id_token"}
+        response = make_response(response)
+        return response, 400
+
     if 'project' in request.json:
         project = request.json['project']
     else:
@@ -14,44 +28,82 @@ def create_project():
         response = make_response(response)
         return response, 400
 
-    my_client = mongoDBInterface.get_db_client()
+    my_client = get_db_client()
+
     if project not in my_client.list_database_names():
-        mongoDBInterface.create_db_for_proj(project)
-        response = {'message': "Created project"}
-        response = make_response(response)
-        return response, 204
+        create_db_for_proj(project)
+        project_user_col = get_col(project, "users")
+        project_user_col.insert_one({'email': requestor_email, 'isAdmin': True, 'isContributor': True})
     else:
         response = {'message': "Project already exists"}
         response = make_response(response)
         return response, 400
 
+    response = {'message': "Created project"}
+    response = make_response(response)
+    return response, 204
+
 
 @project_api.route("/projects", methods=['GET'])
 def get_projects():
-    my_client = mongoDBInterface.get_db_client()
+    my_client = get_db_client()
     names = my_client.list_database_names()
     names.remove("admin")
     names.remove("local")
+    names.remove("users")
     response = {'projects': names}
     response = make_response(response)
     return response, 200
 
 
-@project_api.route("/projects/<project_name>", methods=['DELETE'])
-def delete_project(project_name):
-    my_client = mongoDBInterface.get_db_client()
-    names = my_client.list_database_names()
-    if project_name in names:
-        my_client.drop_database(project_name)
-        response = {'message': "Deleted project"}
+
+@project_api.route("/project/delete", methods=['DELETE'])
+def delete_project():
+    if 'id_token' in request.json:
+        id_token = request.json['id_token']
+
+        requestor_email = get_email(id_token)
+        if requestor_email is None:
+            response = {'message': "ID Token has expired or is invalid"}
+            response = make_response(response)
+            return response, 400
+    else:
+        response = {'message': "Missing id_token"}
         response = make_response(response)
-        return response, 204
+        return response, 400
+
+    if 'project' in request.json:
+        project = request.json['project']
+        if project == "local" or project == "users" or project == "admin":
+            response = {'message': "Cannot delete that project because it is not a user created project"}
+            response = make_response(response)
+            return response, 400
+    else:
+        response = {'message': "Missing projectID"}
+        response = make_response(response)
+        return response, 400
+
+    user_col = get_col(project, "users")
+    requestor = user_col.find_one({'email': requestor_email, 'isAdmin': True})
+    if requestor is None:
+        response = {'message': "Not authorised for that operation"}
+        response = make_response(response)
+        return response, 403
+
+    my_client = get_db_client()
+    names = my_client.list_database_names()
+    if project in names:
+        my_client.drop_database(project)
     else:
         response = {'message': "Project does no exist"}
         response = make_response(response)
         return response, 400
 
+    response = {'message': "Deleted project"}
+    response = make_response(response)
+    return response, 204
+
 
 if __name__ == '__main__':
-    my_client = mongoDBInterface.get_db_client()
+    my_client = get_db_client()
     print(my_client.list_database_names())
