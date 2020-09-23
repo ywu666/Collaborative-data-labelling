b9607038d3562198bbe201b09cec59e0ab7a44a0
Maggie Liu <maggieqliu81@gmail.com>
2020-09-23 15:04:15 +1200

    Add async function

--- app/src/services/ProjectServices.ts
+++ app/src/services/ProjectServices.ts
@@ -9,7 +9,7 @@ export const projectServices = {
     getLabels,
 }
 
-function getProjectNames(firebase: any) {
+async function getProjectNames(firebase: any) {
    const requestOptions = {
        method: 'GET',
        headers: { 'Content-Type': 'application/json', 
@@ -17,8 +17,17 @@ function getProjectNames(firebase: any) {
        "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
        "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With" },
    };
-
-   handleAuthorization(firebase)
+   await handleAuthorization(firebase);
+   const token = localStorage.getItem('user-token');
+   if(firebase.auth.currentUser != null){
+    firebase.auth.currentUser.getIdToken().then((idToken: string) =>{
+        if(token !== idToken){
+            localStorage.setItem('user-token',idToken)
+        }
+       })
+   }else{
+    window.location.href = '/auth';
+   }
 
    return fetch(process.env.REACT_APP_API_URL + '/projects/all?id_token=' + localStorage.getItem('user-token'), requestOptions) // TODO:config.apiUrl
 
@@ -28,7 +37,7 @@ function getProjectNames(firebase: any) {
        })
 }
 
-function getProjectUsers(project: string, firebase: any) {
+async function getProjectUsers(project: string, firebase: any) {
     const requestOptions = {
         method: 'GET',
         headers: { 'Content-Type': 'application/json',
@@ -37,7 +46,7 @@ function getProjectUsers(project: string, firebase: any) {
         "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With" }, 
     };
 
-    handleAuthorization(firebase);
+    await handleAuthorization(firebase);
     
     return fetch(process.env.REACT_APP_API_URL + 
         '/projects/' + project + '/users' + '?id_token=' + localStorage.getItem('user-token'), requestOptions)
@@ -47,15 +56,15 @@ function getProjectUsers(project: string, firebase: any) {
         })
  }
 
- function setProjectUsers(project: string, user: string, firebase:any) {
+ async function setProjectUsers(project: string, user: string, firebase:any) {
     const requestOptions = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json'},
         body: JSON.stringify({ user })
         };
     
-        handleAuthorization(firebase);
-        
+        await handleAuthorization(firebase);
+
     return fetch(process.env.REACT_APP_API_URL + 
         '/projects/' + project + '/users/add' + '?id_token=' + localStorage.getItem('user-token'), requestOptions)
         .then(handleResponse)
@@ -125,7 +134,7 @@ function handleResponse(response: { text: () => Promise<any>; ok: any; status: n
    });
 }
 
-export function handleAuthorization(firebaseAuth: any) {
+export async function handleAuthorization(firebaseAuth: any) {
     const token = localStorage.getItem('user-token');
     if(firebaseAuth.auth.currentUser != null){
         firebaseAuth.auth.currentUser.getIdToken().then((idToken: string) =>{
