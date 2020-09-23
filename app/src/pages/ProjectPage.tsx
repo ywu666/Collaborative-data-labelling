157deec37b201a279db753f3b7b3b9247000493b
tnzhu <tzhu442@aucklanduni.ac.nz>
2020-09-24 01:08:16 +1200

    Resolve merge conflicts

--- app/src/pages/ProjectPage.tsx
+++ app/src/pages/ProjectPage.tsx
@@ -5,74 +5,24 @@ import {
   IonTitle,
   IonToolbar,
   IonButton,
-  IonList,
   IonItem,
-  IonLabel,
   IonIcon,
-  IonModal,
-  IonSkeletonText
 } from '@ionic/react';
-import { add, arrowBack, arrowUpOutline, arrowDownOutline } from 'ionicons/icons';
+import { arrowBack, arrowUpOutline, arrowDownOutline } from 'ionicons/icons';
 import React, {useRef, useState} from 'react';
 import { useParams } from 'react-router';
 import './ProjectPage.css';
-import { isNullOrUndefined } from 'util';
+import * as request from 'request';
+import onLogout from '../helpers/logout'
+import DocumentList from '../components/DocumentList'
 import {projectServices} from "../services/ProjectServices";
 import firebase from "firebase";
-import * as request from 'request';
-import MainPage from './MainPage';
-import onLogout from '../helpers/logout'
-
-
-interface Document {
-  title: string;
-  tag: string;
-}
-
-const sampleDoc: Document[] = [
-  {
-    title: "first doc",
-    tag: "",
-  },
-  {
-    title: "second doc",
-    tag: "tag1",
-  },
-  {
-    title: "long title doc: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    tag: "tag1",
-  },
-]
-
-const labels: string[] = [
-  "tag1",
-  "tag2",
-  "tag3",
-]
 
 const ProjectPage: React.FC = () => {
   const { name } = useParams<{ name: string }>();
-  const [showModal, setShowModal] = useState(false);
-  const [labelIndex, setLabelIndex] = useState(-1);
-  const [isLoading, setIsLoading] = useState(true);
+  const page_size = 10
+  const [pageIndex] = useState(0)
   const inputFile = useRef(null);
-  setTimeout(() => {
-    setIsLoading(false);
-  },
-  1000
-  )
-  const [documents] = useState(sampleDoc); //TODO: get documents via project id
-
-  const renderLabelModal = (i:number) => {
-    setShowModal(true)
-    setLabelIndex(i)
-  }
-
-  const changeTag = (i:number, label:string) => {
-    //TODO: connect with backend to update tags /
-    documents[i].tag = label
-    setShowModal(false)
-  }
 
   const downloadCSV = (projectName:string) => {
     request.get('https://localhost:5000/project/export?project=' + projectName, (response: any) => {
@@ -105,34 +55,7 @@ const ProjectPage: React.FC = () => {
         <div className="container">        
           <h1>{name}</h1>
           <IonButton fill="outline" slot="end" routerLink={"/project/" + name + "/settings"}>Settings</IonButton>
-          <IonModal cssClass="auto-height" isOpen={showModal} onDidDismiss={e => setShowModal(false)}>
-            <div className="inner-content">
-              {labels.map((label, i) =>
-                <IonButton fill="outline" key={i} slot="start" onClick={() => changeTag(labelIndex, label)}>{label}</IonButton>
-              )}
-            </div>
-          </IonModal>
-          {/**
-         * skelenton is displayed if isLoading is true, otherwise document name is displayed
-         */}
-          {isLoading
-          ?<IonList>
-            <IonItem><IonSkeletonText animated style={{ width: '100%' }}></IonSkeletonText></IonItem>
-            <IonItem><IonSkeletonText animated style={{ width: '100%' }}></IonSkeletonText></IonItem>
-            <IonItem><IonSkeletonText animated style={{ width: '100%' }}></IonSkeletonText></IonItem>
-          </IonList>   
-          :<IonList>
-            {documents.map((doc, i) =>
-            <IonItem key={i}>
-              <IonLabel>{doc.title}</IonLabel>
-              {isNullOrUndefined(doc.tag) || doc.tag === ""
-                ? <IonButton fill="outline" slot="end" onClick={() => renderLabelModal(i)}><IonIcon icon={add}/></IonButton>
-                : <IonButton fill="outline" slot="end" onClick={() => renderLabelModal(i)}>{doc.tag}</IonButton>
-              }
-            </IonItem>
-            )}
-            </IonList>
-          }
+          <DocumentList name={name} page={pageIndex} page_size={page_size}/>
         </div>
         <form className="uploadFile" onSubmit={() => {
           // @ts-ignore
