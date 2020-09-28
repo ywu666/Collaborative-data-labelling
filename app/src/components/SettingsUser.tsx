387c3a4d9f4340fa20ff692b4d13dfbabadcc76f
AXon <seismiccat@protonmail.com>
2020-09-28 17:12:47 +1300

    show user status

--- app/src/components/SettingsUser.tsx
+++ app/src/components/SettingsUser.tsx
@@ -1,84 +1,113 @@
 import {
-    IonButton,
-    IonItem,
-    IonLabel,
-    IonAlert
+  IonButton,
+  IonItem,
+  IonLabel,
+  IonAlert
 } from '@ionic/react';
 import React, { useState } from 'react';
 import { projectServices } from '../services/ProjectServices'
 
 interface ContainerProps {
-    project: string;
-    user: string;
-    isContributor: boolean;
-    isAdmin: boolean;
-    firebase:any
+  project: string;
+  user: string;
+  isContributor: boolean;
+  isAdmin: boolean;
+  firebase:any
 }
 
 
 const SettingsUser: React.FC<ContainerProps> = ({ project, user, isContributor, isAdmin, firebase }) => {
 
-    const [showPermissions, setShowPermissions] = useState(false);
+  const [showPermissions, setShowPermissions] = useState(false);
 
-    // set perms here
-    function setPermissions() {
-          projectServices.setUserPermissions(project, user, isAdmin, isContributor, firebase);
-        }
+  const alert = 
+  <IonAlert
+    isOpen={showPermissions}
+    onDidDismiss={() => setShowPermissions(false)}
+    header={'Assign Privileges:'}
+    message={''}
+    inputs={[
+      {
+        type: 'checkbox',
+        label: 'Contributor',
+        value: 'Contributor',
+        checked: isContributor
+      },
+      {
+        type: 'checkbox',
+        label: 'Admin',
+        value: 'Admin',
+        checked: isAdmin
+      }
+    ]}
+    buttons={[
+      {
+        text: 'Cancel',
+        role: 'cancel'
+      },
+      {
+        text: 'Confirm',
+        handler: (data) => {
+          if (data.includes("Contributor")) {
+            isContributor = true;
+          } else {
+            isContributor = false;
+          }
+          if (data.includes("Admin")) {
+            isAdmin = true;
+          } else {
+            isAdmin = false;
+          }
+          setPermissions()
+          }
+      }
+    ]}
+  />
 
+  // set perms here
+  function setPermissions() {
+    projectServices.setUserPermissions(project, user, isAdmin, isContributor, firebase);
+  }
+
+  if (isAdmin) {
     return (
-        <div>
-
-            <IonItem>
-                <IonLabel>{user}</IonLabel>
-                <IonButton fill="clear" slot="end" onClick={() => setShowPermissions(true)}>
-                    Permissions
-                    </IonButton>
-            </IonItem>
-
-            <IonAlert
-                isOpen={showPermissions}
-                onDidDismiss={() => setShowPermissions(false)}
-                header={'Assign Privileges:'}
-                message={''}
-                inputs={[
-                    {
-                        type: 'checkbox',
-                        label: 'Contributor',
-                        value: 'Contributor',
-                        checked: isContributor
-                    },
-                    {
-                        type: 'checkbox',
-                        label: 'Admin',
-                        value: 'Admin',
-                        checked: isAdmin
-                    }
-                ]}
-                buttons={[
-                    {
-                        text: 'Cancel',
-                        role: 'cancel'
-                    },
-                    {
-                        text: 'Confirm',
-                        handler: (data) => {
-                            if (data.includes("Contributor")) {
-                                isContributor = true;
-                            } else {
-                                isContributor = false;
-                            }
-                            if (data.includes("Admin")) {
-                                isAdmin = true;
-                            } else {
-                                isAdmin = false;
-                            }
-                            setPermissions()
-                        }
-                    }
-                ]}
-            />
-        </div>
+      <div>
+        <IonItem>
+          <IonLabel>{user}</IonLabel>
+          <IonButton fill="outline" slot="end" onClick={() => setShowPermissions(true)}>
+            Admin
+          </IonButton>
+        </IonItem>
+        {alert}
+      </div>
     );
+  } else if (isContributor) {
+    return (
+      <div>
+        <IonItem>
+          <IonLabel>{user}</IonLabel>
+          <IonButton fill="outline" slot="end" onClick={() => setShowPermissions(true)}>
+            Contributor
+          </IonButton>                
+        </IonItem>
+
+        {alert}
+      </div>
+      );
+  } else {
+    return (
+      <div>
+        <IonItem>
+          <IonLabel>{user}</IonLabel>
+          <IonButton fill="outline" slot="end" onClick={() => setShowPermissions(true)}>
+            Observer
+          </IonButton>
+        </IonItem>
+
+        {alert}
+      </div>
+    );
+  }
 }
 
 export default SettingsUser;
