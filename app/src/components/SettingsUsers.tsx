8858c88b1009f28963670f4ba07ae71ff185ef71
Maggie Liu <maggieqliu81@gmail.com>
2020-09-30 22:32:47 +1300

    Fix bug on email input

--- app/src/components/SettingsUsers.tsx
+++ app/src/components/SettingsUsers.tsx
@@ -57,6 +57,7 @@ const SettingsUsers: React.FC<ContainerProps> = ({ project, firebase }) => {
     try {
       projectServices.setProjectUsers(project, user, firebase);
       setUsers([...users, {id: 0, email: user, isAdmin: false, isContributor: false}])
+      setNewUser("")
     } catch (e) {
 
     }
@@ -78,9 +79,9 @@ const SettingsUsers: React.FC<ContainerProps> = ({ project, firebase }) => {
             );
           })}
         <IonRow>
-        <IonInput type="text" placeholder="Enter user email..." onIonChange={e => setNewUser(e.detail.value!)}></IonInput>
+        <IonInput value={newUser} type="text" placeholder="Enter user email..." onIonChange={e => setNewUser(e.detail.value!)}></IonInput>
 
-        <IonButton size="small" fill="outline" onClick={() => {
+        <IonButton disabled={newUser == "" || newUser == null} size="small" fill="outline" onClick={() => {
           if (typeof newUser !== 'undefined' && newUser) {
             if (users.some(check => check.email === newUser)) {
               setErrorMessage('User has already been added');
