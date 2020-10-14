6efc75f5c085afc710d9a3394cc168ca36fc4f89
Maggie Liu <maggieqliu81@gmail.com>
2020-10-14 15:55:26 +1300

    remove unused code

--- app/src/components/Download.tsx
+++ app/src/components/Download.tsx
@@ -1,31 +1,11 @@
 import {
-  IonHeader,
-  IonTitle,
-  IonToolbar,
-  IonButton,
-  IonIcon,
   IonToast,
-  IonCard,
-  IonContent,
-  IonPage,
-  IonItem,
-  IonFab,
-  IonFabButton,
   IonLoading
 } from '@ionic/react';
-import { arrowBack } from 'ionicons/icons';
-import onLogout from '../helpers/logout'
-import firebase from "firebase";
-import { arrowUpOutline, arrowDownOutline } from 'ionicons/icons';
-import React, {useEffect, useRef, useState} from 'react';
-import { useParams } from 'react-router';
+import React, {useState} from 'react';
 import { projectServices } from "../services/ProjectServices";
-import { userService } from "../services/UserServices";
-import { documentServices } from "../services/DocumentService"
-import {Tooltip, Fab, Button} from '@material-ui/core';
-import AddIcon from "@material-ui/icons/Add";
+import {Tooltip, Fab} from '@material-ui/core';
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
-import { positions } from '@material-ui/system';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 
@@ -56,29 +36,16 @@ const Download: React.FC<DownloadProps> = (props:DownloadProps) => {
   const [downloadError, setDownloadError] = useState<string>();
   const [isLoading, setIsLoading] = useState(false);
 
-
-
   const downloadCSV = (projectName: string) => {
-
-    console.log("entering CSV")
     setIsLoading(true)
-    console.log(isLoading)
     try {
       projectServices.exportCsv(projectName)
+      setIsLoading(false)
     } catch(e) {
+      setDownloadError("Downloading Error")
+      setIsLoading(false)
     }
-    console.log("finished function csv download")
-    console.log(isLoading)
-    
   }
-
-
-  useEffect(() => {
-    console.log(isLoading)
-    console.log("loading changed")
-    
-  }, [isLoading])
-  
 
   return (
 
@@ -98,19 +65,7 @@ const Download: React.FC<DownloadProps> = (props:DownloadProps) => {
             </Tooltip>
         </MuiThemeProvider>
         
-
-
-
-
-
   );
 }
 
 export default Download;
-/*<IonFab>
-    <form className="downloadFile">
-<IonFabButton onClick={() => downloadCSV(name)}>
-            <IonIcon icon={arrowDownOutline} />
-        </IonFabButton>
-        </form>
-        </IonFab>*/
