028d641223ad3f6dd979fe5599443521ba4bef05
Maggie Liu <maggieqliu81@gmail.com>
2020-10-14 20:23:08 +1300

    Add header to signup and add route back link

--- app/src/pages/SignUpPage.tsx
+++ app/src/pages/SignUpPage.tsx
@@ -11,32 +11,25 @@ import {
   import { useParams } from 'react-router';
   import './LoginPage.css';
   import SignUp from "../components/SignUp";
+import Header from '../components/Header';
   const SignUpPage: React.FC = () => {
     const { name } = useParams<{ name: string }>();
   
     return (
       <IonPage>
-        <IonHeader>
+      <Header name='' routerLink={"/auth"} logout={false}/>
+
+      <IonContent>
+        <IonHeader collapse="condense">
           <IonToolbar>
-            <IonButtons slot="start">
-              <IonMenuButton />
-            </IonButtons>
-            <IonTitle>{name}</IonTitle>
+            <IonTitle size="large">{name}</IonTitle>
           </IonToolbar>
         </IonHeader>
-  
-        <IonContent>
-          <IonHeader collapse="condense">
-            <IonToolbar>
-              <IonTitle size="large">{name}</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-       
-          <div className="login-container">
-            <SignUp />
-          </div>
-        </IonContent>
-      </IonPage>
+        <div className="login-container">
+          <SignUp />
+        </div>
+      </IonContent>
+    </IonPage>
     );
   };
   
